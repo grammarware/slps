267a260f4dd93e1081126533f0bfc8d3812beae1
grammarware <vadim@grammarware.net>
2008-08-19 16:00:55 +0000

    AsFix->BGF part complete
    git-svn-id: https://slps.svn.sourceforge.net/svnroot/slps@184 ab42f6e0-554d-0410-b580-99e487e6eeb2

--- topics/extraction/asfix2btf/post.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import sys
-sys.stdin.readline()
-print '<btf:tree xmlns:btf="http://planet-sl.org/btf" xmlns:bgf="http://planet-sl.org/bgf">'
-for line in sys.stdin:
- print line,
