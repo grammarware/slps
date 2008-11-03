08e22ac667040527aba98477544dd224def3a4c8
grammarware <vadim@grammarware.net>
2008-11-03 17:13:40 +0000

    JLS progress
    git-svn-id: https://slps.svn.sourceforge.net/svnroot/slps@402 ab42f6e0-554d-0410-b580-99e487e6eeb2

--- topics/presentation/analyses/xbgf_check.py
+++ topics/presentation/analyses/xbgf_check.py
@@ -72,9 +72,11 @@ if __name__ == "__main__":
  cx2 = noni(sys.argv[1],incdecxbgf) + nosi(sys.argv[1],'GENERALITY')
  cx3 = nosi(sys.argv[1],'REVISE')
  sum = len(xbgf.findall('/*'))
- print "%3i +%3i +%3i =%4i" % (cx1,cx2,cx3,sum)
+ print "%3i +%3i +%3i =%4i" % (cx1,cx2,cx3,sum),
  if cx1+cx2+cx3!=sum:
+  print 'ERROR'
   sys.exit(1)
  else:
+  print
   sys.exit(0)
 
