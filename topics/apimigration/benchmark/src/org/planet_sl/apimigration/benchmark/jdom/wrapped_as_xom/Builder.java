2a54b6771f6ae606828ffcf0753f352a80037fb2
tvdstorm <storm@cwi.nl>
2009-04-07 18:07:48 +0000

    - Annotated the Builder class.
    git-svn-id: https://slps.svn.sourceforge.net/svnroot/slps@546 ab42f6e0-554d-0410-b580-99e487e6eeb2

--- topics/apimigration/benchmark/src/org/planet_sl/apimigration/benchmark/jdom/wrapped_as_xom/Builder.java
+++ topics/apimigration/benchmark/src/org/planet_sl/apimigration/benchmark/jdom/wrapped_as_xom/Builder.java
@@ -1,6 +1,7 @@
 package org.planet_sl.apimigration.benchmark.jdom.wrapped_as_xom;
 
 import org.planet_sl.apimigration.benchmark.anno.Progress;
+import org.planet_sl.apimigration.benchmark.anno.Wrapping;
 import org.planet_sl.apimigration.benchmark.anno.Progress.Status;
 import org.planet_sl.apimigration.benchmark.anno.Solution;
 import org.planet_sl.apimigration.benchmark.anno.Solution.Strategy;
@@ -13,154 +14,80 @@ import java.io.StringReader;
 
 import org.planet_sl.apimigration.benchmark.anno.MapsTo;
 
+public class Builder {
 
-public class Builder  {
-
+	@Wrapping
 	org.jdom.input.SAXBuilder builder;
-	
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")  Builder(org.jdom.input.SAXBuilder builder)  {
+
+	@Wrapping
+	Builder(org.jdom.input.SAXBuilder builder) {
 		this.builder = builder;
 	}
-	
+
 	// XOM api starts below
-	
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+
+	@Progress(value = Status.OK, comment = "")
+	@Solution(value = Strategy.DELEGATE, comment = "")
 	@MapsTo("org.jdom.input.SAXBuilder()")
-	public  Builder()  {
+	public Builder() {
 		this(new org.jdom.input.SAXBuilder());
 	}
-	
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+
+	@Progress(value = Status.OK, comment = "")
+	@Solution(value = Strategy.DELEGATE, comment = "")
 	@MapsTo("org.jdom.input.SAXBuilder(boolean)")
-	public  Builder(boolean validate)  {
+	public Builder(boolean validate) {
 		this(new org.jdom.input.SAXBuilder(validate));
 	}
-	
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+
+	@Progress(value = Status.GIVENUP, comment = "")
+	@Issue.Pre("NodeFactory is not available in JDOM and cannot be used by SAXBuilder")
 	@MapsTo("")
-	public  Builder(boolean validate, NodeFactory factory)  {
-		throw new UnsupportedOperationException("custom factories not support for JDOM builders"); 
+	public Builder(boolean validate, NodeFactory factory) {
+		throw new UnsupportedOperationException(
+				"custom factories not support for JDOM builders");
 	}
-    
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+
+	@Progress(value = Status.GIVENUP, comment = "")
+	@Issue.Pre("NodeFactory is not available in JDOM and cannot be used by SAXBuilder")
 	@MapsTo("")
-	public  Builder(NodeFactory factory)  {
-		throw new UnsupportedOperationException("custom factories not support for JDOM builders");
+	public Builder(NodeFactory factory) {
+		throw new UnsupportedOperationException(
+				"custom factories not support for JDOM builders");
 	}
-    
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+
+	@Progress(value = Status.NEEDSWORK, comment = "")
+	@Solution(value = Strategy.ADVANCED_DELEGATE, comment = "")
+	@Issue.Pre("Not sure if XMLReader.class can function as a SAX driver")
 	@MapsTo("org.jdom.input.SAXBuilder(String)")
-	public  Builder(org.xml.sax.XMLReader parser)  {
+	public Builder(org.xml.sax.XMLReader parser) {
 		// TODO: don't know about this one...
 		this(new org.jdom.input.SAXBuilder(parser.getClass().getName()));
 	}
 
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+	@Progress(value = Status.NEEDSWORK, comment = "")
+	@Solution(value = Strategy.ADVANCED_DELEGATE, comment = "")
+	@Issue.Pre("Not sure if XMLReader.class can function as a SAX driver")
 	@MapsTo("org.jdom.input.SAXBuilder(String,boolean)")
-	public  Builder(org.xml.sax.XMLReader parser, boolean validate)  {
-		this(new org.jdom.input.SAXBuilder(parser.getClass().getName(), validate));
+	public Builder(org.xml.sax.XMLReader parser, boolean validate) {
+		this(new org.jdom.input.SAXBuilder(parser.getClass().getName(),
+				validate));
 	}
 
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+	@Progress(value = Status.GIVENUP, comment = "")
+	@Issue.Pre("NodeFactory is not available in JDOM and cannot be used by SAXBuilder")
 	@MapsTo("")
-	public  Builder(org.xml.sax.XMLReader parser, boolean validate, NodeFactory factory)  {
-		throw new UnsupportedOperationException("custom factories not support for JDOM builders");
+	public Builder(org.xml.sax.XMLReader parser, boolean validate,
+			NodeFactory factory) {
+		throw new UnsupportedOperationException(
+				"custom factories not support for JDOM builders");
 	}
-	
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+
+	@Progress(value = Status.NEEDSWORK, comment = "")
+	@Solution(value = Strategy.ADVANCED_DELEGATE, comment = "")
+	@Issue.Throws("JDOMException can be thrown without being a JDOMParseException")
 	@MapsTo("org.jdom.input.SAXBuilder#build(String)")
-	public  Document  build(String systemID) throws ParsingException,
-			IOException  {
+	public Document build(String systemID) throws ParsingException, IOException {
 		try {
 			return new Document(builder.build(systemID));
 		} catch (org.jdom.input.JDOMParseException e) {
@@ -171,20 +98,11 @@ public class Builder  {
 		}
 	}
 
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+	@Progress(value = Status.NEEDSWORK, comment = "")
+	@Solution(value = Strategy.ADVANCED_DELEGATE, comment = "")
+	@Issue.Throws("JDOMException can be thrown without being a JDOMParseException")
 	@MapsTo("org.jdom.input.SAXBuilder#build(InputStream)")
-	public  Document  build(InputStream in) throws ParsingException,
-			IOException  {
+	public Document build(InputStream in) throws ParsingException, IOException {
 		try {
 			return new Document(builder.build(in));
 		} catch (org.jdom.input.JDOMParseException e) {
@@ -195,44 +113,27 @@ public class Builder  {
 		}
 	}
 
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+	@Progress(value = Status.NEEDSWORK, comment = "")
+	@Solution(value = Strategy.ADVANCED_DELEGATE, comment = "")
+	@Issue.Throws("JDOMException can be thrown without being a JDOMParseException")
 	@MapsTo("org.jdom.input.SAXBuilder#build(InputStream,String)")
-	public  Document  build(InputStream in, String baseURI) throws ParsingException, IOException  {
+	public Document build(InputStream in, String baseURI)
+			throws ParsingException, IOException {
 		try {
 			// NOTE: in SAXBuilder the second arg here is called systemId
 			return new Document(builder.build(in, baseURI));
 		} catch (org.jdom.input.JDOMParseException e) {
 			throw new ParsingException(e.getMessage(), e.getCause(), baseURI);
-		}
-		catch (org.jdom.JDOMException e) {
+		} catch (org.jdom.JDOMException e) {
 			throw new ParsingException(e.getMessage(), e, baseURI);
 		}
 	}
 
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+	@Progress(value = Status.NEEDSWORK, comment = "")
+	@Solution(value = Strategy.ADVANCED_DELEGATE, comment = "")
+	@Issue.Throws("JDOMException can be thrown without being a JDOMParseException")
 	@MapsTo("org.jdom.input.SAXBuilder#build(File)")
-	public  Document  build(File in) throws ParsingException,
-			IOException  {
+	public Document build(File in) throws ParsingException, IOException {
 		try {
 			return new Document(builder.build(in));
 		} catch (org.jdom.input.JDOMParseException e) {
@@ -243,20 +144,11 @@ public class Builder  {
 		}
 	}
 
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+	@Progress(value = Status.NEEDSWORK, comment = "")
+	@Solution(value = Strategy.ADVANCED_DELEGATE, comment = "")
+	@Issue.Throws("JDOMException can be thrown without being a JDOMParseException")
 	@MapsTo("org.jdom.input.SAXBuilder#build(Reader)")
-	public  Document  build(Reader in) throws ParsingException,
-			IOException  {
+	public Document build(Reader in) throws ParsingException, IOException {
 		try {
 			return new Document(builder.build(in));
 		} catch (org.jdom.input.JDOMParseException e) {
@@ -267,20 +159,12 @@ public class Builder  {
 		}
 	}
 
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+	@Progress(value = Status.NEEDSWORK, comment = "")
+	@Solution(value = Strategy.ADVANCED_DELEGATE, comment = "")
+	@Issue.Throws("JDOMException can be thrown without being a JDOMParseException")
 	@MapsTo("org.jdom.input.SAXBuilder#build(Reader,String)")
-	public  Document  build(Reader in, String baseURI) throws ParsingException,
-			 IOException  {
+	public Document build(Reader in, String baseURI) throws ParsingException,
+			IOException {
 		try {
 			return new Document(builder.build(in, baseURI));
 		} catch (org.jdom.input.JDOMParseException e) {
@@ -291,21 +175,15 @@ public class Builder  {
 		}
 	}
 
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+	@Progress(value = Status.NEEDSWORK, comment = "")
+	@Solution(value = Strategy.ADVANCED_DELEGATE, comment = "")
+	@Issue.Throws("JDOMException can be thrown without being a JDOMParseException")
 	@MapsTo("org.jdom.input.SAXBuilder#build(String,String)")
-	public  Document  build(String document, String baseURI) throws ParsingException, IOException  {
+	public Document build(String document, String baseURI)
+			throws ParsingException, IOException {
 		try {
-			return new Document(builder.build(new StringReader(document), baseURI));
+			return new Document(builder.build(new StringReader(document),
+					baseURI));
 		} catch (org.jdom.input.JDOMParseException e) {
 			throw new ParsingException(e.getMessage(), e.getCause(), baseURI);
 		} catch (org.jdom.JDOMException e) {
@@ -314,20 +192,12 @@ public class Builder  {
 		}
 	}
 
-	@Progress(
-		value = Status.TODO, 
-		comment = ""
-	)
-	@Solution(
-		value = Strategy.OTHER,
-		comment = ""
-	)
-	@Issue.Pre("")
-	@Issue.Post("")
-	@Issue.Throws("")
+	@Progress(value = Status.GIVENUP, comment = "")
+	@Issue.Post("No NodeFactory available since it does not work with SAXBuilder")
 	@MapsTo("")
-	public  NodeFactory  getNodeFactory()  {
-		throw new UnsupportedOperationException("no nodefactories supported in JDOM");
+	public NodeFactory getNodeFactory() {
+		throw new UnsupportedOperationException(
+				"no nodefactories supported in JDOM");
 	}
 
 }
