0a4141411f7ccd40192c4d991ab6b1c43d9acecf
grammarware <vadim@grammarware.net>
2009-04-07 19:09:19 +0000

    trying to make ANTLR extractor somewhat more general
    git-svn-id: https://slps.svn.sourceforge.net/svnroot/slps@548 ab42f6e0-554d-0410-b580-99e487e6eeb2

--- topics/extraction/antlr2bgf/antlrstrip.py
+++ topics/extraction/antlr2bgf/antlrstrip.py
@@ -2,22 +2,31 @@
 import sys
 import re
 
+p0 = re.compile('options [{][^}]*[}]')
+p5 = re.compile('\'}\'')
+p4 = re.compile('tokens [{][^}]*[}]')
 p1 = re.compile('[{][^}]*[}]')
 p2 = re.compile('[@][a-z]*')
 p3 = re.compile('returns [\[][^\]]*[\]]')
+p6 = re.compile('/[*]([^*]|[*][^/])*[*]/')
+p4 = re.compile('tokens [{][^}]*[}]')
 
 def main():
 	if (len(sys.argv)!=3):
-		print "Usage: antlrstrap.py Grammar.g Grammar_pure.g"
+		print "Usage: antlrstrip.py Grammar.g Grammar_pure.g"
 		return
 	inf = open(sys.argv[1],"r")
 	out = open(sys.argv[2],'w')
 	# reading the grammar
 	lines = ' '.join(inf.readlines())
 	# regexp-powered cleaning
-	lines = p1.sub('',lines)
-	lines = p2.sub('',lines)
-	lines = p3.sub('',lines)
+	lines = p6.sub('',lines)	# kill comments
+	lines = p5.sub('',lines)	# kill '}'s
+	lines = p1.sub('',lines)	# kill semantic actions
+	lines = p2.sub('',lines)	# kill modifiers
+	lines = p3.sub('',lines)	# kill returns
+	lines = p0.sub('',lines)	# kill options
+	lines = p4.sub('',lines)	# kill tokens
 	# whitespace shrinking
 	lines = ' '.join(lines.split())
 	# removing terminal symbols, pretty-printing the rest
