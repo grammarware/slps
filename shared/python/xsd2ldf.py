6d865e3176494aa35b004ac4a268ac30f03a9ec2
grammarware <vadim@grammarware.net>
2009-05-19 21:27:51 +0000

    inner documentation gets into place if verticalisation is performed
    git-svn-id: https://slps.svn.sourceforge.net/svnroot/slps@642 ab42f6e0-554d-0410-b580-99e487e6eeb2

--- shared/python/xsd2ldf.py
+++ shared/python/xsd2ldf.py
@@ -1,43 +1,52 @@
 #!/usr/bin/python
 import sys
+import slpsns
 import elementtree.ElementTree as ET
 
-ldfns = 'http://planet-sl.org/ldf'
-bgfns = 'http://planet-sl.org/bgf'
-xbgfns= 'http://planet-sl.org/xbgf'
-xsdns = 'http://www.w3.org/2001/XMLSchema'
-htmlns= 'http://www.w3.org/1999/xhtml'
-
-ET._namespace_map[ldfns] = 'ldf'
-ET._namespace_map[bgfns] = 'bgf'
-ET._namespace_map[xbgfns]='xbgf'
-ET._namespace_map[xsdns] = 'xsd'
-ET._namespace_map[htmlns]='html'
-
-acceptedtags = ('{'+xsdns+'}complexType','{'+xsdns+'}element','{'+xsdns+'}simpleType','{'+xsdns+'}group')
+acceptedtags = (slpsns.xsd_('complexType'),slpsns.xsd_('element'),slpsns.xsd_('simpleType'),slpsns.xsd_('group'))
 
 def mapXSD2LDF(stree,dtree,grammar):
  for nt in stree.findall('/*'):
   if nt.tag not in acceptedtags:
    continue
   section = ET.SubElement(dtree,'core')
-  section.set('id',nt.tag.replace('{'+xsdns+'}','')+'-'+nt.attrib['name'])
+  section.set('id',nt.tag.replace(slpsns.xsd_(''),'')+'-'+nt.attrib['name'])
   #el = ET.SubElement(section,'id')
   #el.text = nt.tag.replace('{'+xsdns+'}','')+'-'+nt.attrib['name']
   el = ET.SubElement(section,'title')
   el.text = nt.attrib['name']
   el = ET.SubElement(section,'description')
   el = ET.SubElement(el,'content')
-  for p in nt.findall('.//{%s}annotation/{%s}documentation' % (xsdns,xsdns)):
+  #for p in nt.findall('.//{%s}annotation/{%s}documentation' % (xsdns,xsdns)):
+  for p in nt.findall(slpsns.xsd_('annotation')+'/'+slpsns.xsd_('documentation')):
    pel = ET.SubElement(el,'text')
    pel.text = p.text
    # e.g. keywords
    for sub in p:
     pel.append(sub)
+  gotit = []
+  if nt.findall(slpsns.xsd_('choice')):
+   print 'Found a top level choice in',nt.get('name')
+   for alt in nt.findall(slpsns.xsd_('choice')+'/*'):
+    if alt.findall(slpsns.xsd_('annotation')+'/'+slpsns.xsd_('documentation')):
+     #print alt.get('name'),'got text!'
+     for p in alt.findall(slpsns.xsd_('annotation')+'/'+slpsns.xsd_('documentation')):
+      pel = ET.SubElement(el,'text')
+      pel.text = p.text
+      # e.g. keywords
+      for sub in p:
+       pel.append(sub)
+     for prod in grammar[nt.get('name')]:
+      if prod.findall('label'):
+       #print 'Checking up on',prod.findtext('label'),'vs',nt.get('name')
+       if prod.findtext('label') == alt.get('name'):
+        el.append(prod)
+        gotit.append(prod)
   # Need to decide whether to put productions inside description subsections
   #section.append(grammar[nt.attrib['name']])
   for prod in grammar[nt.attrib['name']]:
-   el.append(prod)
+   if prod not in gotit:
+    el.append(prod)
   # print grammar[nt.attrib['name']]
 	
 
@@ -48,7 +57,7 @@ def main(xsdfile,bgffile,ldffile):
  dtree = ET.Element('ldf:document')
 
  cx=0
- for prod in gtree.findall('//{%s}production' % bgfns):
+ for prod in gtree.findall('//'+slpsns.bgf_('production')):
   cx+=1
   nt = prod.findtext('nonterminal')
   if nt not in grammar.keys():
@@ -56,15 +65,15 @@ def main(xsdfile,bgffile,ldffile):
   grammar[nt].append(prod)
  print 'Found', cx, 'productions'
 
- dtree.set('xmlns:ldf',ldfns)
- dtree.set('xmlns:bgf',bgfns)
- dtree.set('xmlns:html',htmlns)
+ dtree.set('xmlns:ldf',slpsns.ldfns)
+ dtree.set('xmlns:bgf',slpsns.bgfns)
+ dtree.set('xmlns:html',slpsns.htmlns)
 
  section = ET.SubElement(dtree,'titlePage')
  el = ET.SubElement(section,'author')
  el.text = 'XSD2LDF generator'
  el = ET.SubElement(section,'topic')
- el.text = stree.findall('/{%s}annotation/{%s}documentation' % (xsdns,xsdns))[0].text
+ el.text = stree.findall('/'+slpsns.xsd_('annotation')+'/'+slpsns.xsd_('documentation'))[0].text
  el = ET.SubElement(section,'version')
  el.text = '1.0'
  el = ET.SubElement(section,'status')
@@ -76,28 +85,25 @@ def main(xsdfile,bgffile,ldffile):
  section = ET.SubElement(dtree,'frontMatter')
  el = ET.SubElement(section,'foreword')
  el = ET.SubElement(el,'content')
- for p in stree.findall('/{%s}annotation/{%s}documentation' % (xsdns,xsdns))[1:]:
+ for p in stree.findall('/'+slpsns.xsd_('annotation')+'/'+slpsns.xsd_('documentation'))[1:]:
   pel = ET.SubElement(el,'text')
   pel.text = p.text
 
- if stree.findall('/{'+xsdns+'}import'):
+ if stree.findall('/'+slpsns.xsd_('import')):
   el = ET.SubElement(section,'normativeReferences')
   el = ET.SubElement(el,'content')
   el = ET.SubElement(el,'list')
-  for p in stree.findall('/{'+xsdns+'}import'):
+  for p in stree.findall('/'+slpsns.xsd_('import')):
    pel = ET.SubElement(el,'item')
    pel.text = p.attrib['schemaLocation']
    istree = ET.parse('/'.join(xsdfile.split('/')[:-1])+'/'+p.attrib['schemaLocation'])
    mapXSD2LDF(istree,dtree,grammar)
-  print len(stree.findall('/{'+xsdns+'}import')),'external schema(ta) imported.'
+  print len(stree.findall('/'+slpsns.xsd_('import'))),'external schema(ta) imported.'
 
  mapXSD2LDF(stree,dtree,grammar)
 
  ET.ElementTree(dtree).write(ldffile)
  return
-
-def bgfns_(element):
- return '{'+bgfns+'}'+element
 
 def copymixedcontent (parent, name, stree, xpath):
  element = ET.SubElement(parent, name)
@@ -112,6 +118,7 @@ def copymixedcontent (parent, name, stree, xpath):
 
 if __name__ == "__main__":
  if len(sys.argv) == 4:
+  slpsns.init(ET)
   apply(main,sys.argv[1:4])
  else:
   print '''This tool generates Language Definition Format documents from XML Schema Definitions,
