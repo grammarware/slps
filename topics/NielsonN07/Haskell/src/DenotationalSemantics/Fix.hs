97d72bb9e755922250c7bdd07da1ef50e8d78bc3
rlaemmel <rlaemmel@gmail.com>
2010-01-22 11:45:12 +0000

    git-svn-id: https://slps.svn.sourceforge.net/svnroot/slps@752 ab42f6e0-554d-0410-b580-99e487e6eeb2

--- topics/NielsonN07/Haskell/src/DenotationalSemantics/Fix.hs
+++ topics/NielsonN07/Haskell/src/DenotationalSemantics/Fix.hs
@@ -7,19 +7,30 @@ import Data.Maybe
 
 -- Fixed-point property-based fixed-point combinator
 
+fixProperty :: (x -> x) -> x
 fixProperty f = f (fixProperty f)
-
-
--- Return the list of repeated function applications
-
-repetitions b f x = [ f'i i x | i <- [0..] ]
- where
-  f'i 0 = const b
-  f'i i = f (f'i (i-1))
 
 
 -- Iterative and Maybe-based fixed-point combinator
 
-fixMaybe f x = findJust (repetitions Nothing f x)
+fixMaybe :: ((x -> Maybe x) -> (x -> Maybe x)) -> x -> Maybe x
+fixMaybe f x = iterate (const Nothing)
+ where 
+  iterate g = maybe (iterate (f g)) Just (g x)
+
+
+-- An alternative formulation of fixMaybe
+
+fixMaybe' :: ((x -> Maybe x) -> (x -> Maybe x)) -> x -> Maybe x
+fixMaybe' f x = findJust (repetitions Nothing f x)
  where 
   findJust = head . dropWhile isNothing
+
+
+-- Return the list of repeated function applications
+
+repetitions :: a -> ((b -> a) -> b -> a) -> b -> [a]
+repetitions b f x = [ f'i i x | i <- [0..] ]
+ where
+  f'i 0 = const b
+  f'i i = f (f'i (i-1))
--- topics/NielsonN07/Haskell/src/ProgramAnalysis/Fix.hs
+++ topics/NielsonN07/Haskell/src/ProgramAnalysis/Fix.hs
@@ -9,7 +9,20 @@ import ProgramAnalysis.Domains
 fixEq :: (Bottom x, Eq x)
       => ((x -> x) -> x -> x) -> x -> x
 
-fixEq f x = findEq (repetitions bottom f x)
+fixEq f x = iterate (const bottom)
+ where 
+  iterate r = let r' = f r  in
+               if (r x == r' x) 
+                 then r x
+                 else iterate r'
+
+
+-- An alternative formulation of fixEq
+
+fixEq' :: (Bottom x, Eq x)
+       => ((x -> x) -> x -> x) -> x -> x
+
+fixEq' f x = findEq (repetitions bottom f x)
  where
   findEq (h:h':t)
    = if h==h'
